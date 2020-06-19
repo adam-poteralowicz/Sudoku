7f9fc4ebb499fd30b90d9eaadc95f3ce923ee8c5
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-20 00:01:25 +0200

    Moved method providing test sudoku puzzle to Sudoku class

--- app/src/main/java/com/apap/sudoku/Sudoku.kt
+++ app/src/main/java/com/apap/sudoku/Sudoku.kt
@@ -2,6 +2,20 @@ package com.apap.sudoku
 
 class Sudoku(private val puzzle: Array<IntArray>) {
 
+    companion object {
+
+        fun provideTestSudokuArray() = arrayOf(
+            intArrayOf(0, 0, 1, 0, 0, 0, 0, 0, 0),
+            intArrayOf(2, 0, 0, 0, 0, 0, 0, 7, 0),
+            intArrayOf(0, 7, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(1, 0, 0, 4, 0, 6, 0, 0, 7),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 1, 2, 5, 4, 6),
+            intArrayOf(3, 0, 2, 7, 6, 0, 9, 8, 0),
+            intArrayOf(0, 6, 4, 9, 0, 3, 0, 0, 1),
+            intArrayOf(9, 8, 0, 5, 2, 1, 0, 6, 0))
+    }
+
     fun blanksRemaining(): Int {
         val blanks = (puzzle.indices).flatMap { row ->
             (puzzle[row].indices)
--- app/src/main/java/com/apap/sudoku/activity/SudokuActivity.kt
+++ app/src/main/java/com/apap/sudoku/activity/SudokuActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.apap.sudoku.R
+import com.apap.sudoku.Sudoku
 import com.apap.sudoku.adapter.SudokuRecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_sudoku.*
 
@@ -13,21 +14,9 @@ class SudokuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sudoku)
 
-        val sudokuArray = arrayOf(
-            intArrayOf(0, 0, 1, 0, 0, 0, 0, 0, 0),
-            intArrayOf(2, 0, 0, 0, 0, 0, 0, 7, 0),
-            intArrayOf(0, 7, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(1, 0, 0, 4, 0, 6, 0, 0, 7),
-            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0),
-            intArrayOf(0, 0, 0, 0, 1, 2, 5, 4, 6),
-            intArrayOf(3, 0, 2, 7, 6, 0, 9, 8, 0),
-            intArrayOf(0, 6, 4, 9, 0, 3, 0, 0, 1),
-            intArrayOf(9, 8, 0, 5, 2, 1, 0, 6, 0)
-        )
-
         sudoku_recycler_view.apply {
             layoutManager = GridLayoutManager(this@SudokuActivity, 9)
-            adapter = SudokuRecyclerViewAdapter(sudokuArray)
+            adapter = SudokuRecyclerViewAdapter(Sudoku.provideTestSudokuArray())
         }
     }
 }
