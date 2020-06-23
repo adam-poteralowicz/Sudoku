2a219ff7f4dbd35f2cb503dc6f8e24e357323799
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-23 21:53:14 +0200

    Added dependencies and setup for Robolectric

---
+++ app/src/test/java/com/apap/sudoku/HomeActivityTest.kt
@@ -0,0 +1,10 @@
+package com.apap.sudoku
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class HomeActivityTest {
+
+    
+}
