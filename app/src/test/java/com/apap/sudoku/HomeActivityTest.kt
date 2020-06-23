6cca9c7f487507793410eb663c1948349da920a4
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-24 00:23:32 +0200

    Added example test for HomeActivity using Robolectric

--- app/src/test/java/com/apap/sudoku/HomeActivityTest.kt
+++ app/src/test/java/com/apap/sudoku/HomeActivityTest.kt
@@ -1,10 +1,31 @@
 package com.apap.sudoku
 
+import android.content.Intent
+import android.os.Build
+import com.apap.sudoku.view.game.activity.SudokuActivity
+import com.apap.sudoku.view.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
+import org.junit.Assert
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 class HomeActivityTest {
 
-    
+    @Test
+    fun shouldStartSudokuActivityOnStartGameClick() {
+
+        val activity = Robolectric.setupActivity(HomeActivity::class.java)
+        activity.sudoku_start_button.performClick()
+
+        val expectedIntent = Intent(activity, SudokuActivity::class.java)
+        val actualIntent = Shadows.shadowOf(RuntimeEnvironment.application).nextStartedActivity
+        Assert.assertEquals(expectedIntent.component, actualIntent.component)
+    }
 }
